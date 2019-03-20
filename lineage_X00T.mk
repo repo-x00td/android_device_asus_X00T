7d02b26849534ce2d367725c0da84c33be9a8433
anomalchik <vidslava@yandex.ru>
2019-03-20 12:55:50 -0100

    load device packages before Linage stuff

--- lineage_X00T.mk
+++ lineage_X00T.mk
@@ -19,11 +19,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00T

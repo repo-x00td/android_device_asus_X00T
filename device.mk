98c525b2caa3470b94d1cea1fead4c5bc9db98a6
anomalchik <vidslava@yandex.ru>
2019-05-06 18:48:04 +0000

    Revert "build fingerprint service from source"
    This reverts commit 3bb1b452c37ab9077891c5bcff6513ae4d32ee64.

--- device.mk
+++ device.mk
@@ -163,10 +163,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
 
-# Fingerprint service
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 

021a44b206f8404a639085ce9462e62d219b9ac1
Valeriano <jneto3000@gmail.com>
2019-09-21 15:10:32 -0300

    [DNM] Q: Disable preopt SystemUI for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -279,7 +279,7 @@ WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # Pre-opt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
+#PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
 # inherit from the proprietary version
 -include vendor/asus/X00T/BoardConfigVendor.mk
